a4c677491f053f85f28eedf1967862e2f13267f4
TrombBone <kruchinin-kostya@mail.ru>
2022-05-19 01:16:05 +0300

    Initial commit

---
+++ src/test/kotlin/MyTests.kt
@@ -0,0 +1,47 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MyTests {
+
+    private val needPosition = Board(arrayOf(intArrayOf(1, 2, 3, 4), intArrayOf(5, 6, 7, 8), intArrayOf(9, 10, 11, 12), intArrayOf(13, 14, 15, 0)))
+
+    @Test
+    fun randomTests() {
+        val numbersList = mutableListOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15).shuffled()
+        val numbersArray = Array(4) { IntArray(4) }
+
+        for (i in numbersArray.indices) for (j in numbersArray[i].indices)
+            numbersArray[i][j] = numbersList[i * numbersArray.size + j]
+
+
+        val board = Board(numbersArray)
+//        println(board)
+        val solver = Solver(board)
+
+        if (solver.isSolvable()) {
+            println("isSolvable")
+            assertEquals(needPosition.toString(), solver.solution().last().toString(), "isSolvable")
+        }
+        else {
+            println("isNotSolvable")
+            assertEquals("-1", solver.countMoves().toString(), "isNotSolvable")
+        }
+    }
+
+    @Test
+    fun test19Moves() {
+        val numbersArray: Array<IntArray> = arrayOf(intArrayOf(1, 2, 3, 0), intArrayOf(5, 6, 7, 8), intArrayOf(9, 10, 11, 12), intArrayOf(13, 14, 15, 4))
+
+        val board = Board(numbersArray)
+        val solver = Solver(board)
+
+        if (solver.isSolvable()) {
+            println("isSolvable")
+            assertEquals(needPosition.toString(), solver.solution().last().toString(), "isSolvable")
+        }
+        else {
+            println("isNotSolvable")
+            assertEquals("-1", solver.countMoves().toString(), "isNotSolvable")
+        }
+    }
+}
