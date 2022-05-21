8ce4c9d3ddd073f0cffe370c8c1da7f1a4cea002
vyatkinsen <vyatkinsen@bk.ru>
2022-05-21 22:45:00 +0300

    while fixed

--- src/main/java/ParserForDimacsFiles.java
+++ src/main/java/ParserForDimacsFiles.java
@@ -15,24 +15,21 @@ public class ParserForDimacsFiles {
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line = "";
             while ((line = reader.readLine()) != null) {
+                if (line.endsWith(" 0")) {
+                    Clause currentClause = new Clause();
+                    String[] clauseLineArr = line.substring(0, line.length() - 2).split("\\s+");
+                    Arrays.stream(clauseLineArr)
+                            .filter(literal -> !literal.equals(""))
+                            .forEach(literal -> currentClause.addLiteralToClause(new Literal(Integer.parseInt(literal))));
+                    clauses.add(currentClause);
+                }
                 if (line.startsWith("p")) {
                     String[] paramLineArr = line.split("\\s+");
                     if (!paramLineArr[1].equals("cnf") || paramLineArr[2].equals("0") || paramLineArr[3].equals("0"))
                         throw new IllegalArgumentException("The file has an incorrect format");
                     countOfLiterals = Integer.parseInt(paramLineArr[2]);
                     countOfClauses = Integer.parseInt(paramLineArr[3]);
-                    break;
                 }
-            }
-            while ((line = reader.readLine()) != null) {
-                Clause currentClause = new Clause();
-                if (line.endsWith(" 0")) {
-                    String[] clauseLineArr = line.substring(0, line.length() - 2).split("\\s+");
-                    Arrays.stream(clauseLineArr)
-                            .filter(literal -> !literal.equals(""))
-                            .forEach(literal -> currentClause.addLiteralToClause(new Literal(Integer.parseInt(literal))));
-                }
-                clauses.add(currentClause);
             }
         } catch (IOException e) {
             System.err.println(e.getMessage());
